9c61e4af9a79dd7b18f6fe4140a8c93fab5d61b0
Maazkhan <122012692+Maazkhan2121@users.noreply.github.com>
2023-07-11 02:14:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// const maaz = i=1
+
+// if(maaz<i){
+//     i<2 , i>3 ,i++
+// }
+// else{
+//     i+1
+// }
+// document.getElementById()
+
+
+// var currencies =[
+//     Bitcoin,
+//     Ethereum
+
+// ]
+// currencies.add()
+// console.log(currencies)
